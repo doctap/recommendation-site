9221815723e8a2a9cfd148f03083c85d19d1164f
Jo-x <vasilij.alexsandrovich@gmail.com>
2023-01-15 01:01:27 +0600

    set husky

--- dist/db/requests-db.js
+++ dist/db/requests-db.js
@@ -39,6 +39,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sqlRequest = void 0;
 var pg_1 = require("pg");
 var client = new pg_1.Client({
+    // user: "toor",
+    // password: "toor",
+    // host: "localhost",
     database: 'root_m3iz',
     user: "root",
     password: "fhQJLApGCFbsHb0AXhJ8OasEKVI2aJgn",
--- dist/server.js
+++ dist/server.js
@@ -51,15 +51,29 @@ var manageFS_1 = require("./utils/manageFS");
 var upload_1 = __importDefault(require("./middleware/upload"));
 dotenv_1.default.config();
 var app = (0, express_1.default)();
-var port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 8080; // 8080
+var port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 5000;
 app.use(express_1.default.urlencoded({ extended: true }));
 var corsOptions = {
     credentials: true,
     optionsSuccessStatus: 200,
-    origin: 'https://some-reviews.onrender.com',
+    origin: ['https://some-reviews.onrender.com', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'DELETE'],
 };
-app.use((0, helmet_1.default)());
+app.use((0, helmet_1.default)({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'"],
+            styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
+            imgSrc: ["'self'", 'data:'],
+            connectSrc: ["'self'", 'https://recommendations-app.eu.auth0.com/oauth/token'],
+            fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+            objectSrc: ["'self'"],
+            mediaSrc: ["'self'"],
+            frameSrc: ["'self'", "recommendations-app.eu.auth0.com"],
+        },
+    }
+}));
 app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.static("public"));
 app.use(body_parser_1.default.json());
